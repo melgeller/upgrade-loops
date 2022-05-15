b716101fb1487960ad7402fae96fe6dc8d4bf9b1
melgeller <melgeller.foto@gmail.com>
2022-05-15 14:02:46 +0200

    iteracion3-4-5-6-7

---
+++ iteracion2.js
@@ -0,0 +1,13 @@
+const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+let isApproved = false;
+
+for (i =0; i < alumns.length; i++){
+    if (alumns[i].)
+}
